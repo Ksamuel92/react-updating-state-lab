bd3a5995e5496d1e1b00cfa23e4703f3c0e94129
Kyle Samuel <ksamuel92@gmail.com>
2021-10-20 12:16:59 -0400

    commit tree fe72965afde0ca3f11b58849c982a84b97611886

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,73 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+class YouTubeDebugger extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      },
+    }
+  }
+
+  handleBitRateChange = () => {
+    this.setState({
+      settings: {
+      ...this.state.settings,
+      bitrate: 12
+    }
+    })
+  }
+
+  handleResolutionChange = () => {
+    this.setState({
+      settings: {
+      ...this.state.settings,
+      video: {
+      ...this.state.settings.video,
+      resolution: '720p'
+    }
+  }
+})
+  }
+
+  // handleClick = (e) => {
+  //   // set bitrate to 12
+  //   if (e.target.className === 'bitrate') {
+  //     this.setState({
+  //       settings: {
+  //       ...this.state.settings,
+  //       bitrate: 12
+  //     }
+  //     })
+  //   } else if (e.target.className === 'resolution'){
+  //     this.setState({
+  //       settings: {
+  //       ...this.state.settings,
+  //       video: {
+  //       ...this.state.settings.video,
+  //       resolution: '720p'
+  //     }
+  //   }
+  // })
+  //   }
+  // }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleBitRateChange} className='bitrate'>Bitrate</button>
+        <button onClick={this.handleResolutionChange} className='resolution'>Resolution</button>
+      </div>
+    )
+  }
+}
+
+export default YouTubeDebugger;
